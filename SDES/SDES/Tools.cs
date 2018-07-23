f9245da5d0bf73338b279dff6e1bf4526d4c10f9
Randall Mackin <randall.mackin@oit.edu>
2018-07-23 03:50:48 -0700

    P8, P10, and shift added, plus unit tests for them

--- SDES/SDES/Tools.cs
+++ SDES/SDES/Tools.cs
@@ -3,22 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace SDES
 {
-    class Tools
+    public class Tools
     {
-        
+        public void GenerateKeys(BitArray input)
+        {
+            var output = new BitArray(10); //TODO: Should be a BitArray pair
+            
+        }
         // generateKeys (bitArray input)
 
+        public BitArray PermuteTen(BitArray input)
+        {
+            int[] p10Scramble = { 6, 2, 0, 4, 1, 9, 3, 8, 7, 5 };
+            BitArray output = new BitArray(10);
+
+            for(int i = 0; i < 10; i++)
+            {
+                output[p10Scramble[i]] = input[i];
+            }
+
+            return output;
+        }
         // Permute10 <--
 
+        public BitArray Shift(BitArray input, int shiftCount)
+        {
+            int count = input.Count;
+            BitArray output = new BitArray(count);
+
+            for(int i = 0; i < count; i++)
+            {
+                output[(i + shiftCount) % count] = input[i];
+            }
+
+            return output;
+        }
         // Shift (bitarray input, int shiftCount)
 
+        public BitArray PermuteEight(BitArray input)
+        {
+            int[] p8Scramble = { 1, 3, 5, 0, 2, 4, 7, 6 };
+            BitArray output = new BitArray(8);
+
+            for (int i = 2; i < 10; i++)
+            {
+                output[p8Scramble[i-2]] = input[i];
+            }
+
+            return output;
+        }
         // Permute 8
 
+        public void SplitBitArray(BitArray input)
+        {
+
+        }
         // split
 
+        public void JoinBitArrays(BitArray input) //TODO: Should be a BitArray pair
+        {
+            
+        }
         // join
 
 
---
+++ SDES/SDESTests/ToolsKeyGenTests.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace SDES.Tests
+{
+    [TestClass()]
+    public class ToolsKeyGenTests
+    {
+        Tools testTools;
+        BitArray inputArray;
+        BitArray outputArray;
+
+        [TestInitialize()]
+        public void TestInit()
+        {
+            testTools = new Tools();
+            inputArray = new BitArray(10);
+            outputArray = new BitArray(10);
+        }
+
+        [TestMethod()]
+        public void PermuteTenTestA()
+        {
+            inputArray.Set(0, true);
+            inputArray.Set(1, false);
+            inputArray.Set(2, true);
+            inputArray.Set(3, false);
+            inputArray.Set(4, true);
+            inputArray.Set(5, false);
+            inputArray.Set(6, true);
+            inputArray.Set(7, false);
+            inputArray.Set(8, true);
+            inputArray.Set(9, false);
+
+            outputArray = testTools.PermuteTen(inputArray);
+
+            Assert.AreEqual(true, outputArray[0]);
+            Assert.AreEqual(true, outputArray[1]);
+            Assert.AreEqual(false, outputArray[2]);
+            Assert.AreEqual(true, outputArray[3]);
+            Assert.AreEqual(false, outputArray[4]);
+            Assert.AreEqual(false, outputArray[5]);
+            Assert.AreEqual(true, outputArray[6]);
+            Assert.AreEqual(true, outputArray[7]);
+            Assert.AreEqual(false, outputArray[8]);
+            Assert.AreEqual(false, outputArray[9]);
+        }
+
+        [TestMethod()]
+        public void PermuteTenTestB()
+        {
+            inputArray.Set(0, true);
+            inputArray.Set(1, true);
+            inputArray.Set(2, true);
+            inputArray.Set(3, true);
+            inputArray.Set(4, true);
+            inputArray.Set(5, false);
+            inputArray.Set(6, false);
+            inputArray.Set(7, false);
+            inputArray.Set(8, false);
+            inputArray.Set(9, false);
+
+            outputArray = testTools.PermuteTen(inputArray);
+
+            Assert.AreEqual(true, outputArray[0]);
+            Assert.AreEqual(true, outputArray[1]);
+            Assert.AreEqual(true, outputArray[2]);
+            Assert.AreEqual(false, outputArray[3]);
+            Assert.AreEqual(true, outputArray[4]);
+            Assert.AreEqual(false, outputArray[5]);
+            Assert.AreEqual(true, outputArray[6]);
+            Assert.AreEqual(false, outputArray[7]);
+            Assert.AreEqual(false, outputArray[8]);
+            Assert.AreEqual(false, outputArray[9]);
+        }
+
+        [TestMethod()]
+        public void PermuteEightTestA()
+        {
+            inputArray.Set(0, true);
+            inputArray.Set(1, true);
+            inputArray.Set(2, false);
+            inputArray.Set(3, true);
+            inputArray.Set(4, true);
+            inputArray.Set(5, true);
+            inputArray.Set(6, false);
+            inputArray.Set(7, false);
+            inputArray.Set(8, false);
+            inputArray.Set(9, false);
+
+            outputArray = testTools.PermuteEight(inputArray);
+
+            Assert.AreEqual(true, outputArray[0]);
+            Assert.AreEqual(false, outputArray[1]);
+            Assert.AreEqual(false, outputArray[2]);
+            Assert.AreEqual(true, outputArray[3]);
+            Assert.AreEqual(false, outputArray[4]);
+            Assert.AreEqual(true, outputArray[5]);
+            Assert.AreEqual(false, outputArray[6]);
+            Assert.AreEqual(false, outputArray[7]);
+        }
+
+        [TestMethod()]
+        public void PermuteEightTestB()
+        {
+            inputArray.Set(0, true);
+            inputArray.Set(1, false);
+            inputArray.Set(2, true);
+            inputArray.Set(3, false);
+            inputArray.Set(4, true);
+            inputArray.Set(5, true);
+            inputArray.Set(6, true);
+            inputArray.Set(7, false);
+            inputArray.Set(8, false);
+            inputArray.Set(9, false);
+
+            outputArray = testTools.PermuteEight(inputArray);
+
+            Assert.AreEqual(true, outputArray[0]);
+            Assert.AreEqual(true, outputArray[1]);
+            Assert.AreEqual(true, outputArray[2]);
+            Assert.AreEqual(false, outputArray[3]);
+            Assert.AreEqual(false, outputArray[4]);
+            Assert.AreEqual(true, outputArray[5]);
+            Assert.AreEqual(false, outputArray[6]);
+            Assert.AreEqual(false, outputArray[7]);
+        }
+
+        [TestMethod()]
+        public void ShiftOneTestA()
+        {
+            inputArray = new BitArray(5);
+            inputArray.Set(0, true);
+            inputArray.Set(1, false);
+            inputArray.Set(2, true);
+            inputArray.Set(3, false);
+            inputArray.Set(4, false);
+
+            outputArray = testTools.Shift(inputArray, 1);
+
+            Assert.AreEqual(false, outputArray[0]);
+            Assert.AreEqual(true, outputArray[1]);
+            Assert.AreEqual(false, outputArray[2]);
+            Assert.AreEqual(true, outputArray[3]);
+            Assert.AreEqual(false, outputArray[4]);
+        }
+
+        [TestMethod()]
+        public void ShiftOneTestB()
+        {
+            inputArray = new BitArray(5);
+            inputArray.Set(0, true);
+            inputArray.Set(1, false);
+            inputArray.Set(2, true);
+            inputArray.Set(3, false);
+            inputArray.Set(4, false);
+
+            outputArray = testTools.Shift(inputArray, 2);
+
+            Assert.AreEqual(false, outputArray[0]);
+            Assert.AreEqual(false, outputArray[1]);
+            Assert.AreEqual(true, outputArray[2]);
+            Assert.AreEqual(false, outputArray[3]);
+            Assert.AreEqual(true, outputArray[4]);
+        }
+    }
+}
