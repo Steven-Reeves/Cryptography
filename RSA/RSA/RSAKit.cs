4f1e6be061b29dede979becff7e7b7d57217458f
Randall Mackin <randall.mackin@oit.edu>
2018-08-01 20:56:30 -0700

    added primes file and ability to process this file, P Q and E now generate

--- RSA/RSA/RSAKit.cs
+++ RSA/RSA/RSAKit.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
 
@@ -10,28 +8,68 @@ namespace RSA
 {
     public class RSAKit
     {
-        BigInteger P = 0, Q = 0, E = 0;
+        BigInteger P = new BigInteger(0), Q = new BigInteger(0), E = new BigInteger(0);
+        List<string> _integerList = new List<string>();
 
-        public void GenerateArbitraryKeys()
+        public RSAKit()
+        {
+            _integerList = GetPrimesFromFile("primes.txt");
+        }
+        public List<string> GetPrimesFromFile(string filename)
         {
             var inFile = Directory.GetCurrentDirectory() + "primes.txt";
-            var logFile = File.ReadAllLines(inFile);
-            var primeList = new List<string>(logFile);
+            var logFile = File.ReadAllText(inFile);
 
+            var punctuation = logFile.Where(Char.IsPunctuation).Distinct().ToArray();   //parsing pulled from stackoverflow,
+            var primes = logFile.Split().Select(x => x.Trim(punctuation));              //SUPER COOL
+            List<string> returnedPrimes = new List<string>(primes);
 
+            return returnedPrimes;
+        }
 
+        public void GeneratePQ()
+        {
+            List<int> randomIndexes = new List<int>();
+
+            Random rand = new Random();
+            int temp = rand.Next(_integerList.Count);
+
+            for (int i = 0; i < 2; i++)
+            {
+                while (randomIndexes.Contains(temp) || BigInteger.Parse(_integerList[randomIndexes[temp]]) < BigInteger.Parse("40000"))
+                    temp = rand.Next(_integerList.Count);
+
+                randomIndexes.Add(temp);
+            }
+
+            P = BigInteger.Parse(_integerList[randomIndexes[0]]);
+            Q = BigInteger.Parse(_integerList[randomIndexes[1]]);
         }
 
         public void GenerateKeys()
         {
-            // Generate all keys here
+            GeneratePQ();
+            Create_e();
 
         }
 
         public BigInteger Create_e()
         {
-            // Generate e here
-            return 0;
+            BigInteger E;
+
+            Random random = new Random();
+            int rand = random.Next(_integerList.Count);
+            BigInteger temp = BigInteger.Parse(_integerList[rand]);
+
+            while (temp == P || temp == Q || temp > Phi() || temp < BigInteger.Parse("10000"))
+            {
+                rand = random.Next(_integerList.Count);
+                temp = BigInteger.Parse(_integerList[rand]);
+            }
+
+            E = temp;
+
+            return E;
         }
 
         public BigInteger Create_n()
@@ -41,7 +79,6 @@ namespace RSA
 
         public BigInteger Phi()
         {
-            // Generate n here
             return (P-1) * (Q-1);
         }
 
