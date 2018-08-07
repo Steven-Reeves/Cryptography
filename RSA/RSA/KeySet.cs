c2688618669ffc3ad65f13bc3fecfc94a2a2e5f4
Randall Mackin <randall.mackin@oit.edu>
2018-08-06 17:38:18 -0700

    keyset object added

---
+++ RSA/RSA/KeySet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace RSA
+{
+    public class KeySet
+    {
+        public BigInteger E, D, N;
+
+        public KeySet(BigInteger e, BigInteger d, BigInteger n)
+        {
+            E = e;
+            D = d;
+            N = n;
+        }
+    }
+}
