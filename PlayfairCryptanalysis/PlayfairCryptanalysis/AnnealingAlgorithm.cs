c24e713990ddce5751cded422babacaffa605553
Randall Mackin <randall.mackin@oit.edu>
2018-08-10 15:27:52 -0700

    RandomSwap() added to annealing

--- PlayfairCryptanalysis/PlayfairCryptanalysis/AnnealingAlgorithm.cs
+++ PlayfairCryptanalysis/PlayfairCryptanalysis/AnnealingAlgorithm.cs
@@ -8,5 +8,29 @@ namespace PlayfairCryptanalysis
 {
     class AnnealingAlgorithm
     {
+        string cipherKey = "";
+        Random rand;
+        AnnealingAlgorithm(string key)
+        {
+            cipherKey = key;
+            rand = new Random();
+        }
+
+        string RandomSwap(string key)
+        {
+            var newKey = key.ToCharArray();
+            var index1 = rand.Next(24);
+            var index2 = index1;
+            while (index2 == index1)
+            {
+                index2 = rand.Next(24);
+            }
+
+            var temp = newKey[index1];
+            newKey[index1] = newKey[index2];
+            newKey[index2] = temp;
+
+            return newKey.ToString();
+        }
     }
 }
