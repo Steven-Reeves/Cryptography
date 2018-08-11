66c745f31f008574c27c9d2e14a1607495984a06
Randall Mackin <randall.mackin@oit.edu>
2018-08-11 15:56:49 -0700

    fixed alphabet

--- PlayfairCryptanalysis/PlayfairCryptanalysis/AnnealingAlgorithm.cs
+++ PlayfairCryptanalysis/PlayfairCryptanalysis/AnnealingAlgorithm.cs
@@ -34,7 +34,7 @@ namespace PlayfairCryptanalysis
 
         string RandomKey()
         {
-            List<char> remainingAlphabet = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            List<char> remainingAlphabet = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
             string randomKey = "";
 
             while (remainingAlphabet.Count > 0)
