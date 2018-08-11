1b2ccbe238a5911fb2d769b7954b5e252b181918
Steven Reeves <steven@digieffects.com>
2018-08-11 14:22:16 -0700

    Added intial playfair class implementation

---
+++ PlayfairCryptanalysis/PlayfairCryptanalysisTests/PlayfairTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlayfairCryptanalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayfairCryptanalysis.Tests
+{
+    [TestClass()]
+    public class PlayfairTests
+    {
+        Playfair playfair;
+
+        [TestMethod()]
+        public void PlayfairTest()
+        {
+            playfair = new Playfair("keyword");
+            string expected = "KEYWORDABCFGHILMNPQSTUVXZ";
+            string result = new string(playfair.playfairArray);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod()]
+        public void PlayfairTest2()
+        {
+            playfair = new Playfair("thisisaweirdkeyword");
+            string expected = "THISAWERDKYOBCFGLMNPQUVXZ";
+            string result = new string(playfair.playfairArray);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
